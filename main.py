a43f3989dec356da8a1c9884f5eac75512a21563
yngtzer <tanyngtzer@gmail.com>
2025-10-21 10:31:31 +0800

    init

---
+++ main.py
@@ -0,0 +1,42 @@
+# Set up and run this Streamlit App
+import streamlit as st
+import pandas as pd
+# from helper_functions import llm
+from logics.customer_query_handler import process_user_message
+import streamlit as st  
+from helper_functions.utility import check_password
+
+
+# region <--------- Streamlit App Configuration --------->
+st.set_page_config(
+    layout="centered",
+    page_title="My Streamlit App"
+)
+# endregion <--------- Streamlit App Configuration --------->
+
+st.title("Streamlit App")
+
+# Check if the password is correct.  
+if not check_password():  
+    st.stop()
+
+form = st.form(key="form")
+form.subheader("Prompt")
+
+user_prompt = form.text_area("Enter your prompt here", height=200)
+
+if form.form_submit_button("Submit"):
+    
+    st.toast(f"User Input Submitted - {user_prompt}")
+
+    st.divider()
+
+    response, course_details = process_user_message(user_prompt)
+    st.write(response)
+
+    st.divider()
+
+    print(course_details)
+    df = pd.DataFrame(course_details)
+    df 
+
