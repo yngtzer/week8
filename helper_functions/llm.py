c2faa4f3de5131ae5c2f1d338e051e324b88c93b
yngtzer <tanyngtzer@gmail.com>
2025-10-21 10:43:07 +0800

    update referencing approach

--- helper_functions/llm.py
+++ helper_functions/llm.py
@@ -1,10 +1,15 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from openai import OpenAI
 import tiktoken
 
 
-load_dotenv('.env')
+if load_dotenv('.env'):
+   # for local development
+   OPENAI_KEY = os.getenv('OPENAI_API_KEY')
+else:
+   OPENAI_KEY = st.secrets['OPENAI_API_KEY']
 
 # Pass the API Key to the OpenAI Client
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
